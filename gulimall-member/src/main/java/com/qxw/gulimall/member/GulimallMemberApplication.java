6e419bf046c9a8cac24163341a0fad73c62cf067
qxwupup <877235684@qq.com>
2023-02-02 15:44:48 +0800

    整合nacos为服务注册发现中心

--- gulimall-member/src/main/java/com/qxw/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/qxw/gulimall/member/GulimallMemberApplication.java
@@ -3,7 +3,9 @@ package com.qxw.gulimall.member;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @MapperScan("com.qxw.gulimall.member.dao")
 @SpringBootApplication
 public class GulimallMemberApplication {
